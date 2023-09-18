5e297f1ca5210884355afd2b69afc070d6662432
Ezra.Ombaso <Ezra.Ombaso@LTHSEIT052289>
2023-09-18 11:40:59 +0300

    Add project files.

---
+++ SimpleVideoGameLibrary/Server/Controllers/VideoGameController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using SimpleVideoGameLibrary.Server.Data;
+using SimpleVideoGameLibrary.Shared;
+
+namespace SimpleVideoGameLibrary.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public VideoGameController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<List<VideoGame>>> GetAllVideoGames()
+        {
+            var list = await _context.VideoGames.OrderBy(g => g.ReleaseYear).ToListAsync();
+
+
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<VideoGame>>> CreateVideoGames(VideoGame videoGame)
+        {
+            _context.VideoGames.Add(videoGame);
+            await _context.SaveChangesAsync();
+            
+            return await GetAllVideoGames;
+        }
+    }
+}
